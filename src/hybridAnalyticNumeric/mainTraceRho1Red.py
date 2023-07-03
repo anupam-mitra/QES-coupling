9c7b768256b9d60197cfaef4632875405f24a3e3
Anupam Mitra <Anupam Mitra>
2023-07-03 16:15:17 -0600

    Update pure numeric main.

--- src/hybridAnalyticNumeric/mainTraceRho1Red.py
+++ src/hybridAnalyticNumeric/mainTraceRho1Red.py
@@ -4,13 +4,20 @@ import scipy.special
 import scipy.integrate
 import matplotlib.pyplot as plt
 
-from functions import gFunc, fFunc, trPiece1, trPiece2, normbNeg00, rhoPreFacs
+import logging
+
+from functions import \
+        gFunc, fFunc, trPiece1, trPiece2, normbNeg00, rhoPreFacs, a4bNegFunc
+
+logging.basicConfig(format="%(asctime)s: %(levelname)s: %(message)s", level=logging.DEBUG)
 
 ################################################################################
 if __name__ == '__main__':
     a2idpiov4 = 1
 
     bList = []
+    normList = []
+    a4List = []
 
     trRhoRedPiece1EstList = []
     trRhoRedPiece2EstList = []
@@ -30,21 +37,24 @@ if __name__ == '__main__':
     purityEstList = []
     purityErrList = []
 
-    for i in range(0, 141):
+    for i in range(0, 67):
 
         b2idpiov4 = -(i + 1) * 0.25
-        print("Begin analysis for b = %g" % (b2idpiov4))
+        logging.info("Begin analysis for b = {0}".format(b2idpiov4))
 
         if abs(b2idpiov4/a2idpiov4) < 1:
             INF_APPROX = \
-                4*np.sqrt(3/2*abs(a2idpiov4/b2idpiov4))
+                 3*np.sqrt(3/2*abs(a2idpiov4/b2idpiov4))
 
         else:
             INF_APPROX = \
-                 4*np.sqrt(2/3*abs(b2idpiov4/a2idpiov4))
+                 3*np.sqrt(2/3*abs(b2idpiov4/a2idpiov4))
 
         if b2idpiov4 < 0:
-            a4 = normbNeg00(a2idpiov4, b2idpiov4)**(4.0)
+            norm = normbNeg00(a2idpiov4, b2idpiov4)
+            a4 = a4bNegFunc(a2idpiov4, b2idpiov4)
+            normList.append(norm)
+            a4List.append(a4)
         elif b2idpiov4 > 0:
             pass
         elif b2idpiov4 == 0:
@@ -60,11 +70,11 @@ if __name__ == '__main__':
 
         trRhoRedErr = np.sqrt(trRhoRedPiece1Err**2 + trRhoRedPiece2Err**2)
 
-        print("Probability double-check for b = %g" % (b2idpiov4))
-        print("that trace rho reduced == 1")
-        print("trRhoRedPiece1 = %g +- %g" % (trRhoRedPiece1Est, trRhoRedPiece1Err))
-        print("trRhoRedPiece2 = %g +- %g" % (trRhoRedPiece2Est, trRhoRedPiece2Err))
-        print("trRhoRed = %g +- %g" % (trRhoRedEst, trRhoRedErr))
+        logging.info("Probability double-check for b = {0}".format(b2idpiov4))
+        logging.info("that trace rho reduced == 1")
+        logging.info("trRhoRedPiece1 = %g +- %g" % (trRhoRedPiece1Est, trRhoRedPiece1Err))
+        logging.info("trRhoRedPiece2 = %g +- %g" % (trRhoRedPiece2Est, trRhoRedPiece2Err))
+        logging.info("trRhoRed = %g +- %g" % (trRhoRedEst, trRhoRedErr))
 
         purityPiece1Est, purityPiece1Err = \
             trPiece1(a2idpiov4, b2idpiov4, a4, INF_APPROX)
@@ -75,11 +85,10 @@ if __name__ == '__main__':
         purityEst = purityPiece1Est + purityPiece2Est
         purityErr = np.sqrt(purityPiece1Err**2 + purityPiece2Err**2)
 
-        print("Purity for b = %g" % (b2idpiov4))
-        print("purityPiece1 = %g +- %g" % (purityPiece1Est, purityPiece1Err))
-        print("purityPiece2 = %g +- %g" % (purityPiece2Est, purityPiece2Err))
-        print("purity = %g +- %g" % (purityEst, purityErr))
-
+        logging.info("Purity for b = %g" % (b2idpiov4))
+        logging.info("purityPiece1 = %g +- %g" % (purityPiece1Est, purityPiece1Err))
+        logging.info("purityPiece2 = %g +- %g" % (purityPiece2Est, purityPiece2Err))
+        logging.info("purity = %g +- %g" % (purityEst, purityErr))
         
         bList.append(b2idpiov4)
 
@@ -101,6 +110,31 @@ if __name__ == '__main__':
         purityEstList.append(purityEst)
         purityErrList.append(purityErr)
 
+    fig, axes = plt.subplots(1, 2, figsize=(34.58/2.54, 13.8/2.54))
+
+    axNorm = axes[0]
+    axA4 = axes[1]
+
+    axNormInset = axNorm.inset_axes([0.2, 0.2, 0.6, 0.6])
+    axA4Inset = axA4.inset_axes([0.2, 0.2, 0.6, 0.6])
+
+    axNorm.plot(bList, normList)
+    axA4.plot(bList, a4List)
+
+    axNormInset.plot(bList, normList)
+    axA4Inset.plot(bList, a4List)
+
+    for ax in axes:
+        ax.set_xlabel(r'$b$')
+
+    axNormInset.set_yscale('log')
+    axA4Inset.set_yscale('log')
+
+    axNorm.set_ylabel(r'normbneg00')
+    axA4.set_ylabel(r'$A^4$')
+    plt.savefig("normPlots.pdf")
+
+    plt.close()
         
     fig, axes = plt.subplots(
         2, 3, figsize=(34.58/2.5, 13.8/2.54),
@@ -144,5 +178,5 @@ if __name__ == '__main__':
         ax.axhline(0.5, color='k', alpha=0.5, ls='dashed')
         ax.axhline(1.0, color='k', alpha=0.5, ls='dashed')
 
-
-    plt.savefig('plot.pdf')
+    plt.savefig('tracePlots.pdf')
+    plt.close()
